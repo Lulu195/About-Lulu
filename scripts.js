de7878d993d945be6c5a514d4f8b10c5a11e2903
Constantin <bimasakti.ro@gmail.com>
2024-12-03 01:35:59 +0100

    Add basic webpage structure and styles

---
+++ scripts.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const tagline = document.querySelector(".tagline");
+    tagline.addEventListener("click", function() {
+        alert("Hello, thanks for visiting my website!");
+    });
+});
